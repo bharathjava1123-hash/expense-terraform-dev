49e01a31cfd3b39da69ab13ae6ad78d67ca8a2f7
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-05-03 17:14:14 +0530

    terraform

--- 30-bastion/data.tf
+++ 30-bastion/data.tf
@@ -5,3 +5,26 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
+
+data "aws_ami" "ami_id" {
+
+	owners = ["973714476881"] 
+	
+	filter {
+		name   = "name"
+		values = ["Redhat-9-DevOps-Practice"]
+	}
+	
+	filter {
+		name   = "root-device-type"
+		values = ["ebs"]
+	}
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    most_recent = true
+
+}
--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -2,7 +2,7 @@ module "bastion" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = local.resource_name
-
+  ami = data.aws_ami.ami_id.id
   instance_type = "t3.micro"
   vpc_security_group_ids  = [local.bastion_sg_id]
   subnet_id     = local.public_subnet_id
